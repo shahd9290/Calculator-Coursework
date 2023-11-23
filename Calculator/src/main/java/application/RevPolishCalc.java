284bf442de3547cb5d619b0b2deb3d5e5886422b
Danyal Shah <zlac208@live.rhul.ac.uk>
2023-11-23 19:30:49 +0000

    Started Reverse Polish Calculator Development

---
+++ Calculator/src/main/java/application/RevPolishCalc.java
@@ -0,0 +1,11 @@
+package application;
+
+/**
+ * This class will be used to evaluate a calculation using the Reverse Polish Notation (PostFix).
+ * Each Operator will affect any two preceding Operands, e.g. 1 5 + -> 6.
+ * 
+ * @author Danyal Shah
+ */
+public class RevPolishCalc {
+
+}
---
+++ Calculator/src/test/java/application/TestPostFix.java
@@ -0,0 +1,13 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TestPostFix {
+
+  @Test
+  void testExists() {
+    assertNotNull(new RevPolishCalc());
+  }
+
+}
