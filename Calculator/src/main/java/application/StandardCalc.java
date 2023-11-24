b39cebbb6479df7ee142a52a6d31fec4e9cd1c2c
Danyal Shah <zlac208@live.rhul.ac.uk>
2023-11-24 20:50:32 +0000

    Added Javadoc

--- Calculator/src/main/java/application/StandardCalc.java
+++ Calculator/src/main/java/application/StandardCalc.java
@@ -1,5 +1,11 @@
 package application;
 
+/**
+ * This class will be used to evaluate a calculation using the Standard Infix Notation.
+ * Each operator will be in between the two operands, e.g. 1 + 5 -> 6.
+ * 
+ * @author Danyal Shah
+ */
 public class StandardCalc {
 
 }
