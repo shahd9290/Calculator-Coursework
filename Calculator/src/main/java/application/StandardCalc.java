df594517d49cfb440970ccd75a83e567ae631006
Danyal Shah <zlac208@live.rhul.ac.uk>
2023-11-24 21:50:24 +0000

    Started Standard Addition. Implemented Basic Conversion from Infix to Postfix.

--- Calculator/src/main/java/application/StandardCalc.java
+++ Calculator/src/main/java/application/StandardCalc.java
@@ -1,11 +1,45 @@
 package application;
 
+import java.util.Scanner;
+
 /**
- * This class will be used to evaluate a calculation using the Standard Infix Notation.
- * Each operator will be in between the two operands, e.g. 1 + 5 -> 6.
+ * This class will be used to evaluate a calculation using the Standard Infix Notation. Each
+ * operator will be in between the two operands, e.g. 1 + 5 -> 6.
  * 
  * @author Danyal Shah
  */
 public class StandardCalc {
+
+  RevPolishCalc rpc = new RevPolishCalc();
+  OpStack opStack = new OpStack();
+
+  /**
+   * Performs a calculation on a provided string using the Standard Infix method.
+   * 
+   * @param string The expression to be evaluated.
+   * @return A calculated value based on the expression provided.
+   * @throws InvalidExpression When an expression is entered incorrectly and cannot be performed.
+   */
+
+  public float evaluate(String string) throws InvalidExpression {
+    String expression = "";
+    try (Scanner scan = new Scanner(string)) {
+      while (scan.hasNext()) {
+        String arg = scan.next();
+        if (arg.equals("+")) {
+          opStack.push(Symbol.PLUS);
+        } else {
+          expression = expression + arg + " ";
+        }
+      }
+      while (opStack.size() != 0) {
+        expression = expression + opStack.pop();
+      }
+    } catch (EmptyStackException e) {
+      // TODO Auto-generated catch block
+      return 0;
+    }
+    return rpc.evaluate(expression);
+  }
 
 }
--- Calculator/src/test/java/application/TestInFix.java
+++ Calculator/src/test/java/application/TestInFix.java
@@ -1,16 +1,17 @@
 package application;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestInFix {
 
-  RevPolishCalc sc;
+  StandardCalc sc;
 
   @BeforeEach
   void createSC() {
-    sc = new RevPolishCalc();
+    sc = new StandardCalc();
   }
 
   // Test 1 - Create Class
@@ -19,4 +20,10 @@ class TestInFix {
     assertNotNull(new StandardCalc());
   }
 
+  // Test 2 - Test Basic Addition
+  @Test
+  void testAdditionOne() throws InvalidExpression {
+    assertEquals(8, sc.evaluate("5 + 3"));
+    assertEquals(6, sc.evaluate("3 + 3"));
+  }
 }
