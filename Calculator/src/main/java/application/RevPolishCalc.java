b3b008d6070e82145ffaef3d0073bfbbe14ce5f3
Danyal Shah <zlac208@live.rhul.ac.uk>
2023-11-24 09:24:42 +0000

    Begin Implementing Addition

--- Calculator/src/main/java/application/RevPolishCalc.java
+++ Calculator/src/main/java/application/RevPolishCalc.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Scanner;
+
 /**
  * This class will be used to evaluate a calculation using the Reverse Polish Notation (PostFix).
  * Each Operator will affect any two preceding Operands, e.g. 1 5 + -> 6.
@@ -7,5 +9,20 @@ package application;
  * @author Danyal Shah
  */
 public class RevPolishCalc {
+
+  NumStack numStack = new NumStack();
+  
+  public float evaluate(String string) {
+    Scanner scan = new Scanner(string);
+    while (scan.hasNextInt()) {
+      numStack.push(scan.nextInt());
+    }
+    scan.next();
+    try {
+      return numStack.pop() + numStack.pop();
+    } catch (EmptyStackException e) {
+      return 0;
+    }
+  }
 
 }
--- Calculator/src/test/java/application/TestPostFix.java
+++ Calculator/src/test/java/application/TestPostFix.java
@@ -1,13 +1,36 @@
 package application;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestPostFix {
 
+  RevPolishCalc rpc;
+  
+  @BeforeEach
+  void createRPC() {
+    rpc = new RevPolishCalc();
+  }
+  
+  // Test 1 - Create Class
   @Test
   void testExists() {
     assertNotNull(new RevPolishCalc());
   }
 
+  // Test 2 - Check if correct value is returned.
+  // Fixed by hard coding return value.
+  @Test
+  void testAdditionOne() {
+    assertEquals(3, rpc.evaluate("1 2 +"));
+  }
+  
+  // Test 3 - Check the correct value is returned for different expression
+  // Fixed by using a loop to read each integer in a string, and end when it hits the +.
+  @Test
+  void testAdditionTwo() {
+    assertEquals(5, rpc.evaluate("2 3 +"));
+  }
 }
