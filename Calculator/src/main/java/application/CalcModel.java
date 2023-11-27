1df1b522d7991698110742d6d1f1ff408c09c305
Danyal Shah <zlac208@live.rhul.ac.uk>
2023-11-27 15:49:59 +0000

    Created MockView. Modified CalcModel & CalcController to add views.

--- Calculator/src/main/java/application/CalcModel.java
+++ Calculator/src/main/java/application/CalcModel.java
@@ -4,8 +4,15 @@ package application;
  * Evaluates an expression - the evaluation can be Standard (infix) or reverse polish.
  */
 public class CalcModel implements Calculator {
+  
+  private RevPolishCalc revPolish = new RevPolishCalc();
+  private StandardCalc standard = new StandardCalc();
+  
   @Override
   public float evaluate(String expression, Boolean infix) throws InvalidExpression {
-    return 0.0f;
+    if (infix) {
+      return standard.evaluate(expression);
+    }
+    return revPolish.evaluate(expression);
   }
 }
